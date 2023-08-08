531b3198a12d02e3b3ed1dab22c5cdb161bc53a1
Mel <hakeemt@utexas.edu>
2023-08-08 15:57:59 -0500

    Added functionality to display a single point.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,32 @@ import { Marker } from "react-simple-maps";
 import "react-simple-maps/dist";
 import Destination from "./components/Destination";
 import Plot from "./components/Plot";
+import Point from "./components/Point";
 import airports from "./datasets/airports.json";
 import "./index.scss";
+import { useState } from 'react';
+import Start from "./components/Start";
+import desintationsJson from "./datasets/destinations.json";
+
 
 console.clear();
 
 function App() {
+  const [origin, setOrigin] = useState('New York');
+  const [destination, setDestination] = useState('Tokyo');
+  let originJson = undefined
+  
+  // when there's a val for origin find which city is selected 
+  if (origin) originJson = desintationsJson.find(originSearch => originSearch.name === origin);
+
   return (
     <>
       <div>
-        <Destination name="origin" />
-        <Destination name="dest" />
+        <Destination value={origin} onChange={e => setOrigin(e.target.value)} name="origin" />
+        <Destination value={destination} onChange={e => setDestination(e.target.value)} name="dest" />
+
+
+        {/* <Start cityName=""/> */}
       </div>
 
       <Plot>
@@ -31,6 +46,11 @@ function App() {
             </text>
           </Marker>
         ))}
+        {
+            // if origin json exists return point else nothing
+            originJson && <Point {...originJson} ></Point>
+        }
+
       </Plot>
     </>
   );
