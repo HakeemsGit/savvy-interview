e2151a83d8357be3466b8bceeaa2bc4644f464dd
Mel <hakeemt@utexas.edu>
2023-08-08 16:45:23 -0500

    Path functionality works for close cities, crashes for anything far.

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,37 @@ import { Marker } from "react-simple-maps";
 import "react-simple-maps/dist";
 import Destination from "./components/Destination";
 import Plot from "./components/Plot";
-import Point from "./components/Point";
+import Path from "./components/Path"; 
 import airports from "./datasets/airports.json";
 import "./index.scss";
-import { useState } from 'react';
-import Start from "./components/Start";
-import desintationsJson from "./datasets/destinations.json";
-
+import { useState, useEffect } from 'react';
+import destinationsJson from "./datasets/destinations.json"; 
+import { distance } from './utilities';
 
 console.clear();
+const MAX_DISTANCE = 50.0;
 
 function App() {
   const [origin, setOrigin] = useState('New York');
   const [destination, setDestination] = useState('Tokyo');
-  let originJson = undefined
-  
-  // when there's a val for origin find which city is selected 
-  if (origin) originJson = desintationsJson.find(originSearch => originSearch.name === origin);
+  const [path, setPath] = useState([]);
+
+  useEffect(() => {
+    const originJson = destinationsJson.find(city => city.name === origin);
+    const destJson = destinationsJson.find(city => city.name === destination);
+
+    if (originJson && destJson) {
+      const calculatedPath = calculatePath(originJson, destJson);
+      console.log("Calculated Path:", calculatedPath); // Debugging line
+      setPath(calculatedPath);
+    }
+  }, [origin, destination]);
 
   return (
     <>
       <div>
         <Destination value={origin} onChange={e => setOrigin(e.target.value)} name="origin" />
         <Destination value={destination} onChange={e => setDestination(e.target.value)} name="dest" />
-
-
-        {/* <Start cityName=""/> */}
       </div>
 
       <Plot>
@@ -46,14 +51,46 @@ function App() {
             </text>
           </Marker>
         ))}
-        {
-            // if origin json exists return point else nothing
-            originJson && <Point {...originJson} ></Point>
-        }
-
+        <Path coordinates={path} />
       </Plot>
     </>
   );
+}
+
+function calculatePath(start, end) {
+  let current = start;
+  const path = [start];
+
+  while (distance(current, end) > MAX_DISTANCE) {
+    let nextStop = getNextStop(current, end);
+    if (!nextStop) {
+      console.error("No valid path found");
+      break;
+    }
+    path.push(nextStop);
+    current = nextStop;
+  }
+
+  path.push(end);
+  return path.map(city => [city.x, city.y]); // Return array of coordinates
+}
+
+function getNextStop(current, end) {
+  let closest = null;
+  let minDist = Infinity;
+
+  for (const city of destinationsJson) {
+    if (city === current) continue;
+    const distToCity = distance(current, city);
+    const distCityToEnd = distance(city, end);
+
+    if (distToCity < MAX_DISTANCE && distCityToEnd < minDist) {
+      closest = city;
+      minDist = distCityToEnd;
+    }
+  }
+
+  return closest;
 }
 
 export default App;
